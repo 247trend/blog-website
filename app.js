5e93f9bc9166f825802c0636fed63fdd1d42b2c5
Jason Yuan <jasonyuan0130@gmail.com>
2022-07-11 00:00:07 +1000

    connect mongoDB and minor fix

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//jshint esversion:6
-
 const express = require("express");
 const ejs = require("ejs");
-const _ = require('lodash');
+const _ = require("lodash");
+const connectionUrl = require(__dirname + "/credential.js");
+const mongoose = require("mongoose");
+mongoose.connect(connectionUrl.url + connectionUrl.dbName);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,12 +16,20 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const posts = [];
+const blogSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+});
+const Blog = mongoose.model("Blog", blogSchema);
 
 app.get("/", (req, res) => {
-    res.render("home", {
-        homeStartingContent: homeStartingContent,
-        posts: posts,
+    Blog.find({}, (err, foundBlog) => {
+        if (!err) {
+            res.render("home", {
+                homeStartingContent: homeStartingContent,
+                foundBlog: foundBlog,
+            });
+        }
     });
 });
 
@@ -41,26 +50,37 @@ app.get("/compose", (req, res) => {
 });
 
 app.get("/posts/:title", (req, res) => {
-  posts.forEach(post => {
-    if (_.lowerCase(post.title) === _.lowerCase(req.params.title)) {
-      res.render("post", {
-        postTitle:post.title,
-        postContent:post.content
-      });
-    }
-  });
+    Blog.find({}, (err, foundBlog) => {
+        if (!err) {
+            foundBlog.forEach((post) => {
+                if (_.lowerCase(post.title) === _.lowerCase(req.params.title)) {
+                    res.render("post", {
+                        postTitle: post.title,
+                        postContent: post.content,
+                    });
+                }
+            });
+        }
+    });
 });
 
 app.post("/compose", (req, res) => {
-    const post = {
-        title: req.body.contentTitle,
-        content: req.body.contentBody,
-    };
+    const blogTitle = req.body.contentTitle;
+    const blogContent = req.body.contentBody;
 
-    posts.push(post);
-    res.redirect("/");
+    Blog.create(
+        {
+            title: blogTitle,
+            content: blogContent,
+        },
+        (err) => {
+            if (!err) {
+                res.redirect("/");
+            }
+        }
+    );
 });
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server has started");
 });
